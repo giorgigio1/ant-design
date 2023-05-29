9ec7459ee63d624953a0d7a5790773d1fce57823
giorgigio1 <giorgigiorgdze93@gmail.com>
2023-05-29 16:00:34 +0400

    added table

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Table } from 'antd';
+
+interface dataSourceProps {
+  id: number;
+  key: string;
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    city: string;
+  };
+  phone: string;
+}
+
+interface columnProps {
+  title: string;
+  dataIndex: string;
+  key: string;
+}
 
 function App() {
+  const [dataSource, setDataSource] = useState([
+    {
+      "id": 10,
+      "name": "Vilma Jefferson",
+      "email": "vilmajefferson@gology.com",
+      "gender": "female",
+      "address": {
+        "street": "Bath Avenue",
+        "city": "New York"
+      },
+      "phone": "+1 (814) 496-3905"
+    },
+    {
+      "id": 11,
+      "name": "Cassandra Nguyen",
+      "email": "cassandranguyen@gology.com",
+      "gender": "female",
+      "address": {
+        "street": "Hamilton Avenue",
+        "city": "Chicago"
+      },
+      "phone": "+1 (946) 426-2243"
+    },
+    {
+      "id": 12,
+      "name": "Lenora Clements",
+      "email": "lenoraclements@gology.com",
+      "gender": "female",
+      "address": {
+        "street": "Seba Avenue",
+        "city": "San Diego"
+      },
+      "phone": "+1 (838) 598-2355"
+    },
+    {
+      "id": 13,
+      "name": "Hanson Goodwin",
+      "email": "hansongoodwin@gology.com",
+      "gender": "male",
+      "address": {
+        "street": "Cranberry Street",
+        "city": "New York"
+      },
+      "phone": "+1 (947) 576-2508"
+    },
+  ]);
+
+  const columns = [
+    {
+      key: 1,
+      title: "Id",
+      dataIndex: "id",
+    },
+    {
+      key: 2,
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Email",
+      key: 3,
+      dataIndex: "email",
+    },
+    {
+      title: "Gender",
+      key: 4,
+      dataIndex: "gender",
+    },
+    {
+      title: "Steet",
+      key: 5,
+      dataIndex: "address",
+      render: ({street}:any) => street
+    },
+    {
+      title: "City",
+      key: 6,
+      dataIndex: "city",
+    },
+    {
+      title: "Phone",
+      key: 7,
+      dataIndex: "phone",
+    },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Table dataSource={dataSource} columns={columns} />
     </div>
   );
 }
