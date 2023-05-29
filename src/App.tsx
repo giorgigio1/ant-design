1ba1937aad4c78deccb2bbdf9f274c9023da71f9
giorgigio1 <giorgigiorgdze93@gmail.com>
2023-05-29 19:57:55 +0400

    added "add person button"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css';
-import { useState } from 'react';
-import { Table } from 'antd';
+import "./App.css";
+import { useState } from "react";
+import { Table, Button } from "antd";
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 
-interface dataSourceProps {
+interface DataSourceItem {
   id: number;
-  key: string;
   name: string;
   email: string;
+  gender: string;
   address: {
     street: string;
     city: string;
@@ -14,61 +15,62 @@ interface dataSourceProps {
   phone: string;
 }
 
-interface columnProps {
+interface ColumnItem {
+  key: number;
   title: string;
-  dataIndex: string;
-  key: string;
+  dataIndex?: string;
+  render?: (data: any) => React.ReactNode;
 }
 
 function App() {
-  const [dataSource, setDataSource] = useState([
+  const [dataSource, setDataSource] = useState<DataSourceItem[]>([
     {
-      "id": 10,
-      "name": "Vilma Jefferson",
-      "email": "vilmajefferson@gology.com",
-      "gender": "female",
-      "address": {
-        "street": "Bath Avenue",
-        "city": "New York"
+      id: 10,
+      name: "Vilma Jefferson",
+      email: "vilmajefferson@gology.com",
+      gender: "female",
+      address: {
+        street: "Bath Avenue",
+        city: "New York",
       },
-      "phone": "+1 (814) 496-3905"
+      phone: "+1 (814) 496-3905",
     },
     {
-      "id": 11,
-      "name": "Cassandra Nguyen",
-      "email": "cassandranguyen@gology.com",
-      "gender": "female",
-      "address": {
-        "street": "Hamilton Avenue",
-        "city": "Chicago"
+      id: 11,
+      name: "Cassandra Nguyen",
+      email: "cassandranguyen@gology.com",
+      gender: "female",
+      address: {
+        street: "Hamilton Avenue",
+        city: "Chicago",
       },
-      "phone": "+1 (946) 426-2243"
+      phone: "+1 (946) 426-2243",
     },
     {
-      "id": 12,
-      "name": "Lenora Clements",
-      "email": "lenoraclements@gology.com",
-      "gender": "female",
-      "address": {
-        "street": "Seba Avenue",
-        "city": "San Diego"
+      id: 12,
+      name: "Lenora Clements",
+      email: "lenoraclements@gology.com",
+      gender: "female",
+      address: {
+        street: "Seba Avenue",
+        city: "San Diego",
       },
-      "phone": "+1 (838) 598-2355"
+      phone: "+1 (838) 598-2355",
     },
     {
-      "id": 13,
-      "name": "Hanson Goodwin",
-      "email": "hansongoodwin@gology.com",
-      "gender": "male",
-      "address": {
-        "street": "Cranberry Street",
-        "city": "New York"
+      id: 13,
+      name: "Hanson Goodwin",
+      email: "hansongoodwin@gology.com",
+      gender: "male",
+      address: {
+        street: "Cranberry Street",
+        city: "New York",
       },
-      "phone": "+1 (947) 576-2508"
+      phone: "+1 (947) 576-2508",
     },
   ]);
 
-  const columns = [
+  const columns: ColumnItem[] = [
     {
       key: 1,
       title: "Id",
@@ -80,36 +82,69 @@ function App() {
       dataIndex: "name",
     },
     {
-      title: "Email",
       key: 3,
+      title: "Email",
       dataIndex: "email",
     },
     {
-      title: "Gender",
       key: 4,
+      title: "Gender",
       dataIndex: "gender",
     },
     {
-      title: "Steet",
       key: 5,
+      title: "Steet",
       dataIndex: "address",
-      render: ({street}:any) => street
+      render: ({street}) => street,
     },
     {
-      title: "City",
       key: 6,
-      dataIndex: "city",
+      title: "City",
+      dataIndex: "address",
+      render: ({ city }) => city
     },
     {
-      title: "Phone",
       key: 7,
+      title: "Phone",
       dataIndex: "phone",
     },
-  ]
+    {
+      key: 8,
+      title: "Action",
+      dataIndex: "action",
+      render: () => (
+        <>
+          <EditOutlined onClick={onEditPerson} style={{cursor: 'pointer'}} />
+          <DeleteOutlined style={{color: 'red', marginLeft: 10, cursor: 'pointer'}}  />
+        </>
+      ),
+    },
+  ];
+
+  const onAddPerson =() => {
+    const random = Math.round(Math.random() * 1000)
+    const newItem = {
+      id: dataSource.length + 1,
+      name: random.toString(),
+      email: `${random}@gology.com`,
+      gender: random % 2 === 0 ? 'female' : 'male',
+      address: {
+        street: `${random}th Street`,
+        city: `${random}th City`,
+      },
+      phone: "+1 (814) 496-3905",
+    }
+    setDataSource([...dataSource, newItem]);
+  }
+
+  const onEditPerson = ({id}: any) => {
+    console.log(id);
+  }
 
   return (
     <div className="app">
-      <Table dataSource={dataSource} columns={columns} />
+      <Button onClick={onAddPerson} type="primary" style={{margin: 20}}>Add Person</Button>
+      <Table bordered rowKey="id" columns={columns} dataSource={dataSource} />
     </div>
   );
 }
