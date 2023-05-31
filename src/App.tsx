5e28267ea0779ee21a2ce76be30c68e91d8d4956
giorgigio1 <giorgigiorgdze93@gmail.com>
2023-05-31 16:14:48 +0400

    added Modal window

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState } from "react";
-import { Table, Button } from "antd";
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+import { Table, Button, Modal } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 interface DataSourceItem {
   id: number;
@@ -95,13 +95,13 @@ function App() {
       key: 5,
       title: "Steet",
       dataIndex: "address",
-      render: ({street}) => street,
+      render: ({ street }) => street,
     },
     {
       key: 6,
       title: "City",
       dataIndex: "address",
-      render: ({ city }) => city
+      render: ({ city }) => city,
     },
     {
       key: 7,
@@ -111,40 +111,109 @@ function App() {
     {
       key: 8,
       title: "Action",
-      dataIndex: "action",
-      render: () => (
-        <>
-          <EditOutlined onClick={onEditPerson} style={{cursor: 'pointer'}} />
-          <DeleteOutlined style={{color: 'red', marginLeft: 10, cursor: 'pointer'}}  />
-        </>
-      ),
+      render: (record) => {
+        // console.log(record, "action");
+        return (
+          <>
+            <EditOutlined onClick={showModal} style={{ cursor: "pointer" }} />
+            <DeleteOutlined
+              onClick={() => onDeletePerson(record)}
+              style={{ color: "red", marginLeft: 10, cursor: "pointer" }}
+            />
+          </>
+        );
+      },
     },
   ];
 
-  const onAddPerson =() => {
-    const random = Math.round(Math.random() * 1000)
+  const onAddPerson = () => {
+    const random = Math.round(Math.random() * 1000);
     const newItem = {
       id: dataSource.length + 1,
       name: random.toString(),
       email: `${random}@gology.com`,
-      gender: random % 2 === 0 ? 'female' : 'male',
+      gender: random % 2 === 0 ? "female" : "male",
       address: {
         street: `${random}th Street`,
         city: `${random}th City`,
       },
       phone: "+1 (814) 496-3905",
-    }
+    };
     setDataSource([...dataSource, newItem]);
-  }
+  };
 
-  const onEditPerson = ({id}: any) => {
-    console.log(id);
-  }
+  const onEditPerson = (record: any) => {
+    console.log(record);
+  };
+  const onDeletePerson = (action: any) => {
+    // console.log(action, "delete");
+    setDataSource((pre) => {
+      // console.log(pre)
+      return pre.filter((item) => item.id !== action.id);
+    });
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [value, setValue] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleGenderChange = (event: any) => {
+    setGender(event.target.value);
+  };
+
+  const handleInputChange = (event: any) => {
+    setName(event.target.value);
+    setEmail(event.target.value);
+    setGender(event.target.value);
+    setStreet(event.target.value);
+    setCity(event.target.value);
+    setPhone(event.target.value);
+  };
 
   return (
     <div className="app">
-      <Button onClick={onAddPerson} type="primary" style={{margin: 20}}>Add Person</Button>
+      <Button onClick={showModal} type="primary" style={{ margin: 20 }}>
+        Add Person
+      </Button>
       <Table bordered rowKey="id" columns={columns} dataSource={dataSource} />
+      <Modal
+        title="Modal"
+        open={isModalOpen}
+        onOk={closeModal}
+        onCancel={closeModal}
+        width={800}
+        centered
+      >
+        <label htmlFor="">name</label>
+        <input type="text" value={name} onChange={handleInputChange} />
+        <label htmlFor="">email</label>
+        <input type="text" value={email} onChange={handleInputChange} />
+        <label htmlFor="">gender</label>
+        <select value={gender} onChange={handleInputChange} name="">
+          {gender === "" && <option value="">Select Gender</option>}
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+        <label htmlFor="">street</label>
+        <input type="text" value={street} onChange={handleInputChange} />
+        <label htmlFor="">city</label>
+        <input type="text" value={city} onChange={handleInputChange} />
+        <label htmlFor="">phone</label>
+        <input type="text" value={phone} onChange={handleInputChange} />
+      </Modal>
     </div>
   );
 }
